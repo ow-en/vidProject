b95f69af7ec8f438c35ab88604726ed7a3dbb915
owensource <ciruz001@cougars.csusm.edu>
2018-08-22 16:48:35 -0700

    S2L13 Attribute Routing

--- vidProject/App_Start/RouteConfig.cs
+++ vidProject/App_Start/RouteConfig.cs
@@ -14,6 +14,12 @@ namespace vidProject
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(
+                "MoviesByReleaseDate",
+                "movies/released/{year}/{month}",
+                new { controller = "Movies", action = "ByReleaseDate" },
+                new { year = @"2015|2016|2017|2018", month = @"\d{2}" });
+                
+            routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
